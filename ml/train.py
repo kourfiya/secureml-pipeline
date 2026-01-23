2796dad2aa2e94bc95de8fe3c9250a6b080c91d3
kourfiya <kourfiya.cisse@keysight.com>
2026-01-23 17:40:13 +0000

    fixed conflict

--- ml/train.py
+++ ml/train.py
@@ -21,9 +21,4 @@ accuracy = accuracy_score(y_test, preds)
 print(f"Baseline Accuracy: {accuracy:.4f}")
 
 joblib.dump(model, "ml/model_baseline.joblib")
-<<<<<<< HEAD
 print("Model saved as ml/model_baseline.joblib")
-=======
-print("Model saved as ml/model_baseline.joblib")
-
->>>>>>> test-sast
